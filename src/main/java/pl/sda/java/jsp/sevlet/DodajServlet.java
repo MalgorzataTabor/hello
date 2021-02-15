3b5d24459145e0a7929f7cf8d530640ce4c35f83
Malgorzata Tabor <tabor.malgosia@gmail.com>
2021-02-16 00:55:46 +0100

    new servlet DodajServlet

--- src/main/java/pl/sda/java/jsp/sevlet/DodajServlet.java
+++ src/main/java/pl/sda/java/jsp/sevlet/DodajServlet.java
@@ -6,12 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class Dodaj extends HttpServlet {
+public class DodajServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       String sum1= req.getParameter("sum1");
-       String sum2= req.getParameter("sum2");
-       resp.getWriter().print(Integer.valueOf(sum1) + Integer.valueOf(sum2));
+        String sum1 = req.getParameter("sum1");
+        String sum2 = req.getParameter("sum2");
+        resp.getWriter().print(Integer.valueOf(sum1) + Integer.valueOf(sum2));
     }
 }
